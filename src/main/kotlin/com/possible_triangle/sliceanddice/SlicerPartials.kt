9e06e4c69a12f25c9a3cb06387cd13acef4124cc
Niklas Widmann <niklas.widmann@zuehlke.com>
2023-02-23 20:36:26 +0100

    load partials before modelregistry is frozen

--- src/main/kotlin/com/possible_triangle/sliceanddice/SlicerPartials.kt
+++ src/main/kotlin/com/possible_triangle/sliceanddice/SlicerPartials.kt
@@ -4,6 +4,10 @@ import com.jozufozu.flywheel.core.PartialModel
 
 object SlicerPartials {
 
+    fun load() {
+        // Only here so the class gets loaded
+    }
+
     val SLICER_HEAD = PartialModel(Content.modLoc("block/slicer/head"))
 
 }
