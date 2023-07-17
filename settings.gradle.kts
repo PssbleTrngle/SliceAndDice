567d5939c9a230ad237f25f15b8071ba23e9f398
Niklas Widmann <nik@somethingcatchy.net>
2023-07-17 21:44:43 +0200

    remove local maven reference

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,10 +5,6 @@ pluginManagement {
         gradlePluginPortal()
         maven { url = uri("https://maven.minecraftforge.net/") }
         maven { url = uri("https://repo.spongepowered.org/repository/maven-public/") }
-        
-        System.getenv()["LOCAL_MAVEN"]?.let { localMaven ->
-            maven { url = uri(localMaven) }
-        }
     }
 }
 
