33f4c4535969ec49d9eb11258a875821fc3849b0
Niklas Widmann <nik@somethingcatchy.net>
2023-07-17 21:53:15 +0200

    use published plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,12 +3,8 @@ val mod_name: String by extra
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven {
-            url = uri("https://maven.minecraftforge.net/")
-        }
-        maven {
-            url = uri("https://repo.spongepowered.org/repository/maven-public/")
-        }
+        maven { url = uri("https://maven.minecraftforge.net/") }
+        maven { url = uri("https://repo.spongepowered.org/repository/maven-public/") }
     }
 }
 
