c3b06d9934358d7a0dddaca0e36b9438ec8967b0
Niklas Widmann <nik@somethingcatchy.net>
2023-08-03 13:42:00 +0200

    lazy cooking recipe conversion with automatic fluid detection

--- src/main/kotlin/com/possible_triangle/sliceanddice/config/ServerConfig.kt
+++ src/main/kotlin/com/possible_triangle/sliceanddice/config/ServerConfig.kt
@@ -10,7 +10,7 @@ class ServerConfig(builder: ForgeConfigSpec.Builder) {
 
     val BASIN_COOKING = builder.define("basin_cooking.enabled", true)
     //val REPLACE_FLUID_CONTAINERS = builder.define("basin_cooking.replace_fluid_containers", true)
-    val REPLACE_FLUID_CONTAINERS = Supplier { false }
+    val REPLACE_FLUID_CONTAINERS = Supplier { true }
 
     val SPRINKLER_CAPACITY = builder.defineInRange("sprinkler.fluid_capacity", 300, 10, 2048)
     val SPRINKLER_USAGE = builder.defineInRange("sprinkler.fluid_per_use", 100, 0, 2048)
